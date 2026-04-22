b552fe5ed8cd1b0add581ca5eeda637b9f9eb62c
melizahaiziK <melizaha1zik@gmail.com>
2026-04-22 10:16:57 +0700

    jawaban 2.1.1 no. 5 modifikasi code

--- MahasiswaDemo11.java
+++ MahasiswaDemo11.java
@@ -11,6 +11,7 @@ public class MahasiswaDemo11 {
             System.out.println("3. Melihat Tugas Teratas");
             System.out.println("4. Melihat Daftar Tugas");
             System.out.println("5. Melihat Tugas Terbawah");
+            System.out.println("6. Jumlah Tugas");
             System.out.print("Pilih : ");
             pilih = sc.nextInt();
             sc.nextLine();
@@ -52,6 +53,9 @@ public class MahasiswaDemo11 {
                     if (bawah != null){
                         System.out.println("Tugas pertama dikumpulkan oleh " + bawah.nama);
                     }
+                    break;
+                case 6:
+                    System.out.println("Jumlah tugas saat ini adalah: " + stack.count());
                     break;
                 default:
                     System.out.println("Pilihan tidak valid.");
